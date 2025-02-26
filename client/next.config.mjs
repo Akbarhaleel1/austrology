f734cbfca3165ad7479874cb0a6532eac28536c4
akbar-haleel <akbarhaleel@ikm.gov.in>
2025-02-27 02:28:14 +0530

    sds

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: "/:path*",
+          has: [{ type: "protocol", value: "http" }],
+          destination: "https://austrology.vercel.app/:path*",
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
