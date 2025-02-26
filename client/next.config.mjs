94f9a642851d85db59e566c541ac792077c59526
akbar-haleel <akbarhaleel@ikm.gov.in>
2025-02-27 02:30:47 +0530

    sds

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,18 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// export default nextConfig;
-
-import { NextResponse } from "next/server";
-
-export function middleware(req) {
-  const url = req.nextUrl;
-
-  // If request is HTTP, redirect to HTTPS
-  if (url.protocol === "http:") {
-    url.protocol = "https:";
-    return NextResponse.redirect(url);
-  }
-
-  return NextResponse.next();
-}
+export default nextConfig;
