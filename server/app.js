d19f8030845a8ce246f13e729e262afcdada8dd3
akbar-haleel <akbarhaleel@ikm.gov.in>
2025-02-28 00:13:26 +0530

    port

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const { getAccessToken } = require("./contorller");
 const { getKundliData, getCalendarData, getInauspiciousPeriod } = require("./components/components");
 const app = express();
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
