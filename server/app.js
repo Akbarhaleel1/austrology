e822386b5c4e08a8d5d3fd19802e7180132cb10e
akbar-haleel <akbarhaleel@ikm.gov.in>
2025-02-28 09:58:47 +0530

    changeind

--- server/app.js
+++ server/app.js
@@ -89,5 +89,5 @@ app.get("/calendar", async (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running onnn http://localhost:${PORT}`);
 });
